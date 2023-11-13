221219cc7f81b7432886809985dc36148bc6fae0
Smitkumar Contractor <smitcontractor@gmail.com>
2023-11-13 11:34:32 -0800

    Initial commit

---
+++ tik-tok-ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  tik-tok-ios
+//
+//  Created by Smitkumar Contractor on 11/13/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ tik-tok-ios/tik_tok_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  tik_tok_iosApp.swift
+//  tik-tok-ios
+//
+//  Created by Smitkumar Contractor on 11/13/23.
+//
+
+import SwiftUI
+
+@main
+struct tik_tok_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
